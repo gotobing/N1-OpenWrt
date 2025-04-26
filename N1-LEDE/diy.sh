641aa0b4a231c27d721d9ffa54f9389fbf273da9
Googlex <you@example.com>
2025-04-26 13:22:37 +0800

    🚀添加ios设备共享网络支持

--- N1-LEDE/diy.sh
+++ N1-LEDE/diy.sh
@@ -39,5 +39,9 @@ rm -rf feeds/luci/applications/luci-app-mosdns
 # Default IP
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 
+# usbmuxd开机启动->ios设备通信
+sed -i '/exit 0/i usbmuxd' package/base-files/files/etc/rc.local
+
 #修改默认时间格式
 sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $(find ./package/*/autocore/files/ -type f -name "index.htm")
+
